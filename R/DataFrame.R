770244d35528c76c55cb4483d760ef8926332428
rishu <rishabhrathore055@gmail.com>
2022-12-18 10:21:57 +0530

    Learn how to make df in r

---
+++ R/DataFrame.R
@@ -0,0 +1,12 @@
+id <- c(1:10)
+
+
+name <- c("John Mendes", "Rob Stewart", "Rachel Abrahamson", "Christy Hickman", "Johnson Harper", "Candace Miller", "Carlson Landy", "Pansy Jordan", "Darius Berry", "Claudia Garcia")
+
+
+job_title <- c("Professional", "Programmer", "Management", "Clerical", "Developer", "Programmer", "Management", "Clerical", "Developer", "Programmer")
+
+
+employee <- data.frame(id, name, job_title)
+
+head(employee)
