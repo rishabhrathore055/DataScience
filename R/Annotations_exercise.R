5904906b73d5f4cdda2a3b734f3e6e582952c839
rishu <rishabhrathore055@gmail.com>
2022-12-29 16:50:50 +0530

    learn how to use Markdown documentation in R

---
+++ R/Annotations_exercise.R
@@ -0,0 +1,35 @@
+hotel_bookings <- read.csv("hotel_bookings.csv")
+View(hotel_bookings)
+head(hotel_bookings)
+colnames(hotel_bookings)
+library(ggplot2)
+library(tidyverse)
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel)
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  labs(title="")
+
+mindate <- min(hotel_bookings$arrival_date_year)
+maxdate <- max(hotel_bookings$arrival_date_year)
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       subtitle=paste0("Data from: ", mindate, " to ", maxdate))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       caption=paste0("Data from: ", mindate, " to ", maxdate),
+       x="Market Segment",
+       y="Number of Bookings")
+ggsave('hotel_booking_chart.png', width=7,
+       height=7)
