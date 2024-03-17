7229389209086adce837715a378b02cd6d9332f5
rishu <rishabhrathore055@gmail.com>
2024-03-17 21:30:21 +0530

    Module dir Created

---
+++ Module/main.py
@@ -0,0 +1,12 @@
+import mymodule
+from mymodule import courses
+mymodule.greeting("Rishabh")
+
+print(mymodule.person1['name'])
+
+course = courses['DA']
+print(course)
+
+import datetime
+
+print(datetime.datetime.now())
---
+++ Module/mymodule.py
@@ -0,0 +1,15 @@
+def greeting(name):
+  print("Hello, " + name+ " Have a Nice Day")
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+courses = {
+  'DA' : 35,
+  'CS'  : 180,
+  'IT'   :70
+}
